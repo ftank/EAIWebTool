e0b9fdb2500a51462065dc9119812e50d06e1b21
SCARANELLO TANK, FELIPE <ft0290@us.att.com>
2019-03-21 13:03:46 +0100

    Tool to Monitor and upgrade STBS remotely

---
+++ RackChecker/js/upgrader.js
@@ -0,0 +1,100 @@
+
+$("#submitrelease").click(function () {
+    release = true
+    rackname = $("#multiple option:selected").val();
+    path = $("#releasein").val();
+    stb = parseInt($("#multiple option:selected").attr("nb"));
+    downloadbuild(rackname, path, release);
+    checkbox(stb, release);
+
+});
+
+
+$("#submitnightly").click(function () {
+    release = false
+    rackname = $("#multiple option:selected").val();
+    path = $("#nightlyin").val();
+    stb = parseInt($("#multiple option:selected").attr("nb"));
+    downloadbuild(rackname, path, release);
+    checkbox(stb, release);
+
+});
+$("#upgraderelease").hide();
+$("#upgradenightly").hide();
+$("#upgraderelease").click(function () {
+    rackname = $("#multiple option:selected").val();
+    version = ($("#releasein").val().split( "/" )[ 1 ]);
+    var checkboxValues = [];
+    $('input:checked').map(function () {
+        checkboxValues.push($(this).attr('id'));
+    });
+    console.log(rackname + ' ' + checkboxValues + ' -v ' + version + ' -i ')
+
+});
+
+$("#upgradenightly").click(function () {
+    rackname = $("#multiple option:selected").val();
+    version = ($("#nightlyin").val().split( "/" )[ 1 ]);
+    var checkboxValues = [];
+    $('input:checked').map(function () {
+        checkboxValues.push($(this).attr('id'));
+    });
+    console.log(rackname + ' ' + checkboxValues + ' -v ' + version + ' -i ')
+
+});
+
+function downloadbuild(rackname, path, release) {
+
+    if (release == true) {
+        command = rackname + ' -r ' + path;
+    }
+    else {
+        command = rackname + path;
+    }
+    $.ajax({
+        type: "GET",
+        dataType: 'text',
+        url: "http://172.25.11.43:3000/index/downloadbuild/" + command,
+        success: function (data) {
+            if (release == true) {
+                $("#releaseout").append("<b> Downloading the build to rack " + rackname + "<br></b>")
+                $("#releaseout").append(data);
+            }
+            else {
+                $("#nightlyout").append("<b> Downloading the build to rack" + rackname + "<br></b>")
+                $("#nightlyout").append(data);
+            }
+
+        },
+        error: function (error) {
+
+            if (release == true) {
+                $("#releaseout").append("<b>Error to download the build on Rack " + rackname + "<br></b>");
+            }
+            else
+                $("#nightlyout").append("<b> Error to download the build on Rack " + rackname + "<br></b>")
+        }
+    });
+}
+
+function checkbox(stb, release) {
+
+    if (release == true) {
+        $("#release").append("Select the STBs to be upgrade:<br>")
+        for (i = 1; i < stb + 1; i++) {
+            $("#release").append('<input type="checkbox" id=' + i + '>' + i)
+
+        }
+        $("#upgraderelease").show()
+
+    }
+    else {
+        $("#nightly").append("Select the STBs to be upgrade:<br>")
+        for (i = 1; i < stb + 1; i++) {
+            $("#nightly").append('<input type="checkbox" id=' + i + '>' + i)
+
+        }
+        $("#nightly").show()
+    }
+
+}
---
+++ RackChecker/web.js
@@ -0,0 +1,244 @@
+// nodejs modules
+var shell = require('shelljs')
+
+var express = require('express'),
+  app = express()
+port = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
+app.use(express.static(__dirname));
+app.listen(port);
+
+app.get('/index/eq0210*', function (req, res) {
+
+  var rackname = "eq0210"
+  child = shell.exec('/home/fscaranellotank/Desktop/test.sh ' + rackname + req.params[0], { async: true });
+  child.stdout.on('data', function (data) {
+    res.send(data);
+  });
+});
+
+app.get('/index/eq0211*', function (req, res) {
+
+  var rackname = "eq0211"
+  child = shell.exec('/home/fscaranellotank/Desktop/test.sh ' + rackname + req.params[0], { async: true });
+  child.stdout.on('data', function (data) {
+    res.send(data);
+  });
+});
+
+app.get('/index/eq0212*', function (req, res) {
+
+  var rackname = "eq0212"
+  child = shell.exec('/home/fscaranellotank/Desktop/test.sh ' + rackname + req.params[0], { async: true });
+  child.stdout.on('data', function (data) {
+    res.send(data);
+  });
+});
+
+app.get('/index/downloadbuild/eq0212*', function (req, res) {
+
+  var rackname = "eq0212"
+  child = shell.exec('/home/fscaranellotank/Desktop/downloadbuild.sh ' + rackname + req.params[0], { async: true });
+  child.stdout.on('data', function (data) {
+    res.send(data);
+  });
+});
+
+app.get('/index/eq0214*', function (req, res) {
+
+  var rackname = "eq0214"
+  child = shell.exec('/home/fscaranellotank/Desktop/test.sh ' + rackname + req.params[0], { async: true });
+  child.stdout.on('data', function (data) {
+    res.send(data);
+  });
+});
+
+app.get('/index/eq0215*', function (req, res) {
+
+  var rackname = "eq0215"
+  child = shell.exec('/home/fscaranellotank/Desktop/test.sh ' + rackname + req.params[0], { async: true });
+  child.stdout.on('data', function (data) {
+    res.send(data);
+  });
+});
+
+app.get('/index/eq0216*', function (req, res) {
+
+  var rackname = "eq0216"
+  child = shell.exec('/home/fscaranellotank/Desktop/test.sh ' + rackname + req.params[0], { async: true });
+  child.stdout.on('data', function (data) {
+    res.send(data);
+  });
+});
+
+app.get('/index/eq0217*', function (req, res) {
+
+  var rackname = "eq0217"
+  child = shell.exec('/home/fscaranellotank/Desktop/test.sh ' + rackname + req.params[0], { async: true });
+  child.stdout.on('data', function (data) {
+    res.send(data);
+  });
+});
+
+app.get('/index/eq0307*', function (req, res) {
+
+  var rackname = "eq0307"
+  child = shell.exec('/home/fscaranellotank/Desktop/test.sh ' + rackname + req.params[0], { async: true });
+  child.stdout.on('data', function (data) {
+    res.send(data);
+  });
+});
+
+app.get('/index/eq0308*', function (req, res) {
+
+  var rackname = "eq0308"
+  child = shell.exec('/home/fscaranellotank/Desktop/test.sh ' + rackname + req.params[0], { async: true });
+  child.stdout.on('data', function (data) {
+    res.send(data);
+  });
+});
+
+app.get('/index/getter/eq0308*', function (req, res) {
+
+  var rackname = "eq0308"
+  child = shell.exec('/home/fscaranellotank/Desktop/getter.sh ' + rackname + req.params[0], { async: true });
+  child.stdout.on('data', function (data) {
+    res.send(data);
+  });
+});
+
+
+app.get('/index/eq0309*', function (req, res) {
+
+  var rackname = "eq0309"
+  child = shell.exec('/home/fscaranellotank/Desktop/test.sh ' + rackname + req.params[0], { async: true });
+  child.stdout.on('data', function (data) {
+    res.send(data);
+  });
+});
+
+app.get('/index/eq0317*', function (req, res) {
+
+  var rackname = "eq0317"
+  child = shell.exec('/home/fscaranellotank/Desktop/test.sh ' + rackname + req.params[0], { async: true });
+  child.stdout.on('data', function (data) {
+    res.send(data);
+  });
+});
+
+app.get('/index/eq0318*', function (req, res) {
+
+  var rackname = "eq0318"
+  child = shell.exec('/home/fscaranellotank/Desktop/test.sh ' + rackname + req.params[0], { async: true });
+  child.stdout.on('data', function (data) {
+    res.send(data);
+  });
+});
+
+app.get('/index/eq0319*', function (req, res) {
+
+  var rackname = "eq0319"
+  child = shell.exec('/home/fscaranellotank/Desktop/test.sh ' + rackname + req.params[0], { async: true });
+  child.stdout.on('data', function (data) {
+    res.send(data);
+  });
+});
+
+app.get('/index/eq0320*', function (req, res) {
+
+  var rackname = "eq0320"
+  child = shell.exec('/home/fscaranellotank/Desktop/test.sh ' + rackname + req.params[0], { async: true });
+  child.stdout.on('data', function (data) {
+    res.send(data);
+  });
+});
+
+app.get('/index/e05*', function (req, res) {
+
+  var rackname = "e05"
+  child = shell.exec('/home/fscaranellotank/Desktop/test.sh ' + rackname + req.params[0], { async: true });
+  child.stdout.on('data', function (data) {
+    res.send(data);
+  });
+});
+
+app.get('/index/e06*', function (req, res) {
+
+  var rackname = "e06"
+  child = shell.exec('/home/fscaranellotank/Desktop/test.sh ' + rackname + req.params[0], { async: true });
+  child.stdout.on('data', function (data) {
+    res.send(data);
+  });
+});
+
+app.get('/index/e07*', function (req, res) {
+
+  var rackname = "e07"
+  child = shell.exec('/home/fscaranellotank/Desktop/test.sh ' + rackname + req.params[0], { async: true });
+  child.stdout.on('data', function (data) {
+    res.send(data);
+  });
+});
+
+app.get('/index/f02*', function (req, res) {
+
+  var rackname = "f02"
+  child = shell.exec('/home/fscaranellotank/Desktop/test.sh ' + rackname + req.params[0], { async: true });
+  child.stdout.on('data', function (data) {
+    res.send(data);
+  });
+});
+
+app.get('/index/f03*', function (req, res) {
+
+  var rackname = "f03"
+  child = shell.exec('/home/fscaranellotank/Desktop/test.sh ' + rackname + req.params[0], { async: true });
+  child.stdout.on('data', function (data) {
+    res.send(data);
+  });
+});
+
+app.get('/index/f04*', function (req, res) {
+
+  var rackname = "f04"
+  child = shell.exec('/home/fscaranellotank/Desktop/test.sh ' + rackname + req.params[0], { async: true });
+  child.stdout.on('data', function (data) {
+    res.send(data);
+  });
+});
+
+app.get('/index/f05*', function (req, res) {
+
+  var rackname = "f05"
+  child = shell.exec('/home/fscaranellotank/Desktop/test.sh ' + rackname + req.params[0], { async: true });
+  child.stdout.on('data', function (data) {
+    res.send(data);
+  });
+});
+
+app.get('/index/f07*', function (req, res) {
+
+  var rackname = "f07"
+  child = shell.exec('/home/fscaranellotank/Desktop/test.sh ' + rackname + req.params[0], { async: true });
+  child.stdout.on('data', function (data) {
+    res.send(data);
+  });
+});
+
+app.get('/index/f08*', function (req, res) {
+
+  var rackname = "f08"
+  child = shell.exec('/home/fscaranellotank/Desktop/test.sh ' + rackname + req.params[0], { async: true });
+  child.stdout.on('data', function (data) {
+    res.send(data);
+  });
+});
+
+
+console.log("Server Started!!");
